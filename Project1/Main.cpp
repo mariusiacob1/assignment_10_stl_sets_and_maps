cc822695d70115a3948925608d7a5e6240701bcf
mariusiacob1 <47167361+mariusiacob1@users.noreply.github.com>
2020-04-11 21:10:33 -0700

    A10 Complete

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -1,3 +1,12 @@
+/*
+	Iacob, Marius
+
+	March 11, 2020
+
+	CS A250
+	Assignment 10 STL Vectors and Lists
+*/
+
 #include <iostream>
 #include <string>
 #include <set>
@@ -17,9 +26,7 @@ void printSet(const set<int>& aSet)
 	{
 		cout << i << " ";
 	}
-	cout << endl;
 }
-
 
 int main()
 {
@@ -43,6 +50,7 @@ int main()
 	// Print the set using the printSet function you implemented.
 	cout << "set1: ";
 	printSet(set1);
+	cout << endl;
 
 	// What do you notice in the printout?
 		// The numbers were all in order. Not 2, 7, 5, 6, 9, 1 and 3.
@@ -70,10 +78,10 @@ int main()
 	{
 		cout << " " << *iter;
 	}
+	cout << endl;
 
 	// If you do not know what the return value is, then
 	// check set::erase in cplusplus.com
-	cout << endl;
 
 	// Print set1.
 	cout << "set1: ";
@@ -87,7 +95,9 @@ int main()
 	// Delete the second element in the set without creating
 	// an iterator variable, but do use the prefix increment
 	// operator.
-
+	cout << "set1 (ERASE 2nd element): ";
+	set<int>::iterator iter = ++set1.begin();
+	set1.erase(iter);
 
 	// Print set1.
 	printSet(set1);
@@ -95,11 +105,14 @@ int main()
 
 	// pair<iterator,bool> insert (const value_type& val);
 	// Use the function insert to insert 4 and 8 in set1.
-
-
+	cout << "set1 (INSERT 4 & 8): ";
+	pair<int, int> pair1 = make_pair(4, 8);
+	set1.insert(pair1.first);
+	set1.insert(pair1.second);
 
 	// Print set1.
-
+	printSet(set1);
+	cout << endl;
 
 	set<int>::iterator first = set1.begin();
 	set<int>::iterator second = ++set1.begin();
@@ -107,10 +120,11 @@ int main()
 	// Use function erase to delete the first and second element
 	// in set1. Use the given iterators created above.
 	// Note that you should write one statement only.
-
+	cout << "set1 (ERASE FIRST & SECOND): ";
+	set1.erase(first, ++second);
 
 	// Print set1.
-
+	printSet(set1);
 
 	// Your output should be: 5 6 7 8
 	// If it is not, you need to carefully view the function erase
@@ -130,12 +144,61 @@ int main()
 	// Use the following functions to manipulate the maps:
 	// pair<iterator,bool> insert (const value_type& val);
 	// void insert (InputIterator first, InputIterator last);
-	// iterator  erase(const_iterator position);
+	// iterator erase(const_iterator position);
 	// size_type erase(const key_type& k);
-	// iterator  erase(const_iterator first, const_iterator last);
+	// iterator erase(const_iterator first, const_iterator last);
 	// Print each map without creating a print function, but by using
 	// a loop.
 
+	map<int, string> map1; // Model & car
+
+	cout << "map1: ";
+	pair<int, string> p1;
+	p1 = make_pair(5052, "Nissan");
+	map1.insert(p1);
+	map1[2434] = "Subaru";
+	map1[2301] = "Toyota";
+	map1[3401] = "Ferrari";
+	
+	map<int, string>::iterator itFirst = map1.begin();
+	map<int, string>::iterator itSecond = map1.begin();
+	itSecond++;
+	itSecond++;
+	map1.erase(itFirst, itSecond);
+	map1.erase(3401);
+
+	for (auto i : map1) // Printing
+	{
+		cout << "(" << i.first << ", " << i.second << ") ";
+	}
+	cout << endl;
+	
+	// Copy map1 to map2
+	cout << "map2: ";
+	map<int, string> map2(map1);
+	
+	for (auto i : map2) // Printing
+	{
+		cout << "(" << i.first << ", " << i.second << ") ";
+	}
+	cout << endl;
+
+	cout << "map3: ";
+	map<int, string> map3 // ID & Nickname
+	{
+		{31, "Jeff"}, 
+		{22, "Mar"},
+		{35, "Claud"},
+		{2, "Dave"}
+	};
+	map<int, string>::iterator iterMap3 = map3.begin();
+
+	map3.erase(iterMap3++);
+
+	for (auto i : map3) // Printing 
+	{
+		cout << "(" << i.first << ", " << i.second << ")";
+	}
 
 	cout << "\n\n----------------------------------------------------";
 
@@ -143,5 +206,3 @@ int main()
 	system("Pause");
 	return 0;
 }
-
-// Definition function printSet
